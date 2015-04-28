7e059dbd06be37866eaaa027df60816148bea8b5
kevinlin <linjiang1205@qq.com>
2015-04-28 09:30:27 +0800

    优化input列表，使可以自由定义多少个参数

--- example/first_test/js/basic.js
+++ example/first_test/js/basic.js
@@ -15,18 +15,52 @@ $(document).ready(function(){
         $(this).next(".sublist").toggleClass("hidden");
     });
 
-    //$(".result").text("怎么就找不到你呢");
+    //注册当输入框有东西时，提交按钮亮起事件
+    showSubmitWhenInputSomething();
 
-
+    //注册点击提交事件
+    registerSumbit();
 });
 
 var funcContext = {
     funcList:[
-        {"name": "经验", "id": "exp", "desc": "填写经验数值", "url": "/cgi-bin/webtool?cmd=2", "input1": "exp"},
-        {"name": "金币", "id": "rmb", "desc": "填写提交数值", "url": "/cgi-bin/webtool?cmd=2", "input1": "rmb"},
-        {"name": "vip经验", "id": "vip_gold", "desc": "填写vip经验数值", "url": "/cgi-bin/webtool?cmd=2", "input1": "vip_exp"},
-        {"name": "铜板", "id": "gold", "desc": "填写要加的铜板数值", "url": "/cgi-bin/webtool?cmd=2", "input1": "copper"},
-        {"name": "体力", "id": "physical", "desc": "体力写在这里啊", "url": "/cgi-bin/webtool?cmd=2", "input1": "physical"},
+        {"name": "经验", "id": "exp",  "url": "/cgi-bin/webtool?cmd=2",
+            "inputList": [
+                {"name" : "exp", "desc": "填写经验数值"}
+            ]
+        },
+        {"name": "金币", "id": "rmb", "url": "/cgi-bin/webtool?cmd=2",
+            "inputList": [
+                {"name": "rmb", "desc": "填写加要的金币数值"}
+            ]
+        },
+        {"name": "vip经验", "id": "vip_gold", "url": "/cgi-bin/webtool?cmd=2",
+            "inputList": [
+                {"name": "vip_exp", "desc" : "填写vip经验数值"}
+            ]
+        },
+
+        {
+            "name": "铜板", "id": "gold",  "url": "/cgi-bin/webtool?cmd=2",
+            "inputList": [
+                {"name": "copper", "desc": "填写要加的铜板数值"}
+            ]
+        },
+
+        {
+            "name": "体力", "id": "physical", "url": "/cgi-bin/webtool?cmd=2",
+            "inputList": [
+                {"name": "physical", "desc": "要加的体力写在这里"}
+            ]
+        },
+
+        {"name": "我是测试条", "id": "test", "url": "/cgi-bin/webtool?cmd=2",
+            "inputList": [
+                {"name": "testinput1", "desc": "testinput1"},
+                {"name": "testinput2", "desc": "testinput2"},
+                {"name": "testinput3", "desc": "testinput3"}
+            ]
+        },
     ]
 };
 
@@ -56,72 +90,76 @@ findParamName = function(id, param){
         }
     }
 };
+
+registerSumbit = function() {
 //注册点击提交的事件
-$(".funcBtn").click(function(){
-    var input = $(this).parent().find("input").first();
-    console.log("vaule:" + input.val());
-    var cgi = input.data("url");
-    console.dir("input:" + input.attr("id"));
-    var paramName = findParamName(input.attr("id"), "input1");
-    console.log(paramName);
-    if(typeof paramName != "undefined")
-    {
-        cgi += "&" + paramName + "=" + input.val();
-    }
-    else
-    {
-        console.log("not found paramName：" + "input1");
-    }
-    console.log(cgi);
-
-    //获得父页面中选择的玩家的信息
-//        var player = window.parent.$(".main_select");
-    var player = window.parent.$(":selected");
-    if(typeof player == "undefined") {
-        console.log("parent main_select not found") ;
-    }else{
-        console.dir("player:" + player);
-        console.log("parent main_select found, select " + player.val()) ;
-        cgi += "&player_id=" + player.attr("player_id");
-        cgi += "&zone_id=" + player.attr("zone_id");
+    $(".funcBtn").click(function () {
+        var input = $(this).parent().find("input").first();
+        console.log("vaule:" + input.val());
+        var cgi = $(this).parent().find("label").first().data("url");
+        console.log("cgi:" + cgi)
+        var paramName = findParamName(input.attr("id"), "input1");
+        console.log(paramName);
+        if (typeof paramName != "undefined") {
+            cgi += "&" + paramName + "=" + input.val();
+        }
+        else {
+            console.log("not found paramName：" + "input1");
+        }
         console.log(cgi);
-    }
 
-    //去请求cgi
-    $.get(cgi, function(data){
-    }).success(function(data){
-        console.log("req succ, rsp:" + data)
-        //var result_p =  $(this).parent().find("#result").first();
-        //result_p.text(data);
-        //result_p.removeClass("hidden");
-        window.parent.$("#result").text(data.toString());
-    }).error(function(data, status){
-        console.log("req failed, status:" + status + " data:" + data);
+        //获得父页面中选择的玩家的信息
+//        var player = window.parent.$(".main_select");
+        var player = window.parent.$(":selected");
+        if (typeof player == "undefined") {
+            console.log("parent main_select not found");
+        } else {
+            console.dir("player:" + player);
+            console.log("parent main_select found, select " + player.val());
+            cgi += "&player_id=" + player.attr("player_id");
+            cgi += "&zone_id=" + player.attr("zone_id");
+            console.log(cgi);
+        }
 
-        //data="testt.......";
-        //result_p.removeClass("hidden");
+        //去请求cgi
+        $.get(cgi, function (data) {
+        }).success(function (data) {
+            console.log("req succ, rsp:" + data)
+            //var result_p =  $(this).parent().find("#result").first();
+            //result_p.text(data);
+            //result_p.removeClass("hidden");
+            window.parent.$("#result").text(data.toString());
+        }).error(function (data, status) {
+            console.log("req failed, status:" + status + " data:" + data);
 
-        //console.dir("this:" + $(this).parent());
-        //var result_p =  $(this).parent().find(".result");
-        //result_p.text(".....q");
+            //data="testt.......";
+            //result_p.removeClass("hidden");
 
-        //console.dir("next:" + $(this).parent());
-        //$(this).next("p").text("this is next");
-        //window.parent.$("#result").text(data.toString());
-        window.parent.$("#result").text("这里展示结果。");
+            //console.dir("this:" + $(this).parent());
+            //var result_p =  $(this).parent().find(".result");
+            //result_p.text(".....q");
+
+            //console.dir("next:" + $(this).parent());
+            //$(this).next("p").text("this is next");
+            //window.parent.$("#result").text(data.toString());
+            window.parent.$("#result").text("这里展示结果。");
+        });
+
+        //$("p").text("怎么就找不到你呢");
+
     });
-
-    //$("p").text("怎么就找不到你呢");
-
-});
+}
 
 //input中有输入时，才使提交亮起
-$("input").keyup(function(){
+showSubmitWhenInputSomething = function() {
+    $("input").keyup(function () {
 //        console.log("input value is " + $(this).first().val());
-    if($(this).val() == ""){
-        $(this).parent().find("button").first().attr("disabled", true);
-    }
-    else{
-        $(this).parent().find("button").first().attr("disabled", false);
-    }
-});
+        //FIXME 当所有的都有输入时才亮起来
+        if ($(this).val() == "") {
+            $(this).parent().find("button").first().attr("disabled", true);
+        }
+        else {
+            $(this).parent().find("button").first().attr("disabled", false);
+        }
+    });
+}
