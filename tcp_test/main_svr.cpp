80035dafb32a4316a6c6b2467745abaaf9c98a9b
kevinlin <linjiang1205@qq.com>
2016-03-19 10:21:06 +0800

    for test remote

--- tcp_test/main_svr.cpp
+++ tcp_test/main_svr.cpp
@@ -3,10 +3,15 @@
 #include <assert.h>
 
 
-int main()
+int main(int argc, char** argv)
 {
+    if(argc < 3) {
+        printf("Usage:%s listen_ip listen_port\n", argv[0]);
+        return -1;
+    }
+
     tcp_server svr;
-    int rv = svr.init("127.0.0.1", 1986);
+    int rv = svr.init(argv[1], atoi(argv[2]));
     assert(0 == rv);
 
     svr.test_loop();
